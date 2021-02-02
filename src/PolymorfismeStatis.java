05f1573aeb9c26d79143967f8f02e8c1b4ecddf8
081332620512 <febrimozak@gmail.com>
2021-02-02 19:27:30 +0700

    first commit

---
+++ src/PolymorfismeStatis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PolymorfismeStatis {
+    // method menghitung luas dengan jari-jari
+    float luas(float r){
+    return (float) (Math.PI * r * r);
+    }
+    // method menghitung luas dengan diameter
+    double luas(double d){
+    return (double) (1/4 * Math.PI * d);
+
+    }  
+}
